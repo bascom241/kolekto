921d8cc1f74c510ca28b0381ed275eaf5dfb9484
Ghazali Habeebulahi <gazaliantech@gmail.com>
2025-05-08 12:39:43 +0100

    Update userSchema.js
    back to normal

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,58 +1,74 @@
-import mongoose from 'mongoose';
-import validator from 'validator';
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcryptjs";
 
-const phoneRegex = /^(?:\+234|0)[789][01]\d{8}$/;
 
+const phoneRegex = /^(?:\+234|0)[789][01]\d{8}$/; 
 const userSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: [true, 'Please provide full name'],
-    trim: true
-  },
-  email: {
-    type: String,
-    required: [true, 'Please provide email'],
-    unique: true,
-    validate: [validator.isEmail, 'Please provide a valid email']
-  },
-  password: {
-    type: String,
-    required: [true, 'Please provide password'],
-    select: false
-  },
-  confirmPassword: {
-    type: String,
-    required: [true, 'Please confirm password']
-  },
-  phoneNumber: {
-    type: String,
-    required: [true, 'Please provide phone number'],
-    validate: {
-      validator: function(v) {
-        return phoneRegex.test(v);
-      },
-      message: props => `${props.value} is not a valid phone number!`
-    }
-  },
-  verificationToken: {
-    type: String
-  },
-  verificationTokenExpiresDate: {
-    type: Date
-  },
-  isVerified: {
-    type: Boolean,
-    default: false
-  },
-  resetPasswordToken: {
-    type: String
-  },
-  resetPasswordExpiresDate: {
-    type: Date
-  }
-}, {
-  timestamps: true
-});
+    fullName:{
+        type:String,
+        required:[true, "Please provide your Full name"],
+        trim:true,
+    },
+    email:{
+        type:String,
+        required:[true, "Please Provide Your Email"],
+        unique:true,
+        validate:{
+            validator:function(v){
+                return validator.isEmail(v)
+            },
+            message:(props) => `${props.value} is not a valid email!`
+        }
+    },
+    password:{
+        type:String,
+        required:[true, "Please Provide Your Password"],
+        minlength:6,
+        select:false
+    },
+    confirmPassword:{
+        type:String,
+        // required:[true, "Please Provide Your Password"]
 
-const User = mongoose.model('User', userSchema);
+    },
+    phoneNumber:{
+        type:String,
+        required:[true, "Please Provide Your Phone Number"],
+        unique:true,
+        validate:{
+            validator:function(v){
+                return phoneRegex.test(v)
+            },
+            message:(props) => `${props.value} is not a valid phone number!`
+        }
+    },
+    role:{
+        type:String,
+        enum:["user", "admin"],
+        default:"user"
+    },
+    isVerified:{
+        type:Boolean,
+        default:false
+    },
+    lastLogin:{
+        type:Date,
+        default:Date.now()
+    },
+    resetPasswordToken:String,
+    resetPasswordExpiresDate:Date,
+    verificationToken:String,
+    verificationTokenExpiresDate:Date
+},{timestamps:true});
+
+
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+      this.password = await bcrypt.hash(this.password, 10);
+    }
+    this.confirmPassword = undefined;
+    next();
+  });
+const User = mongoose.model("User", userSchema);
 export default User;
